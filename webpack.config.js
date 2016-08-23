3d158be967cb03fde864c5cb91895d30d0175b16
Fred Wang <namelos@outlook.com>
2016-08-23 21:33:52 +0800

    setup stylus

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  devtool: 'eval-source-map',
   entry: './src/index',
   output: {
     path: 'dist',
@@ -11,11 +12,22 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin({
+      multiStep: true
     })
   ],
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel' }
+      { test: /\.jsx?$/, loader: 'babel' },
+      { test: /\.styl$/, loader: 'style!css!stylus' }
     ]
-  }
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    stats: 'errors-only',
+    port: 3000
+  },
 }
